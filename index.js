dcc0cfa1899597adbbdacbfddac5d586006c598c
sid-FBLA <sid20102004@gmail.com>
2022-06-18 16:22:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const StockXData = require("./src/stockx");
+const stockX = new StockXData();
+
+stockX.searchProducts("Jordan 1 Clay Green").then((searchedProduct) => {
+  console.log(searchedProduct)
+});
 
 module.exports = StockXData;
